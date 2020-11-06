314748a32ceee79173e67680c58ef2626b833486
Nevm PC <Nevm PC@DESKTOP-EUP8F13>
2020-11-06 19:50:18 +0200

    FIRSR COMITВ

---
+++ public_html/courseWork/js/script.js
@@ -0,0 +1,160 @@
+
+
+let bool=1;
+
+
+
+
+function clicknext(button){
+    let aside_block= document.querySelector('.aside_block')
+
+
+if(bool==1){
+aside_block.innerHTML=`<aside class="aside " id="menu1">
+<ul>
+  <li>
+      <a class="tabnav" href="tabs/menutabs/tab1.html">Зима</a>
+      
+        <div class="wblock">
+       
+        <ul class="col">
+        <li>
+        <ul class="hor">
+        <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, aspernatur veniam quisquam error exercitationem modi molestias beatae fuga optio debitis.</div></li>
+    <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quidem earum exercitationem voluptatibus architecto omnis sequi consectetur corporis impedit velit.</div></li>
+    <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum consectetur quibusdam quasi aliquid dolore sed magnam sint repellendus. Veritatis, ipsa.</div></li>
+    <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi ad distinctio atque assumenda illo fugiat ducimus provident suscipit, adipisci aliquam!</div></li>
+      </ul>
+        </li>
+        <li>
+          <ul class="hor">
+            <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, aspernatur veniam quisquam error exercitationem modi molestias beatae fuga optio debitis.</div></li>
+        <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quidem earum exercitationem voluptatibus architecto omnis sequi consectetur corporis impedit velit.</div></li>
+        <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum consectetur quibusdam quasi aliquid dolore sed magnam sint repellendus. Veritatis, ipsa.</div></li>
+        <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi ad distinctio atque assumenda illo fugiat ducimus provident suscipit, adipisci aliquam!</div></li>
+          </ul>
+        </li>
+
+
+          
+          <a class="more" href="tabs/menutabs/tab1.html">Детальніше...</a>
+          </div>
+        
+      
+  </li>
+  <li>
+      <a  class="tabnav"href="tabs/menutabs/tab2.html">Літо</a>
+      <ul>
+        <div class="wblock"> <ul>
+          <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor, </div></li>
+          <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor </div></li>
+          <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor </div></li>
+          <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor </div></li>
+         
+        </ul></div>
+        
+      </ul>
+  </li>
+  <li>
+      <a class="tabnav"href="tabs/menutabs/tab3.html">Осінь</a>
+      <ul>
+        <div class="wblock"> <ul>
+          <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor</div></li>
+          <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor</div></li>
+          <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor</div></li>
+          <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor</div></li>
+         
+        </ul></div>
+        
+      </ul>
+  </li>
+  <li>
+    <a class="tabnav"href="tabs/menutabs/tab4.html">Весна</a>
+    <ul>
+      <div class="wblock"> <ul>
+        <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor</div></li>
+        <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor </div></li>
+        <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor </div></li>
+        <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor </div></li>
+       
+      </ul></div>
+      
+    </ul>
+</li>
+<li>
+  <a class="tabnav" href="tabs/menutabs/tab5.html">Спорт</a>
+  <ul>
+    <div class="wblock"> <ul>
+      <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor</div></li>
+      <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor </div></li>
+      <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor </div></li>
+      <li><div><img src="https://i.pinimg.com/originals/f4/d2/96/f4d2961b652880be432fb9580891ed62.png" alt="img">Lorem ipsum dolor</div></li>
+     
+    </ul></div>
+    
+  </ul>
+</li>
+</ul> 
+</aside>`;
+bool--;
+}
+else {aside_block.innerHTML=`<h1></h1>`;
+bool++}
+
+}
+
+
+function item(id){
+
+localStorage.setItem("id",id)
+
+}
+
+
+
+function getdata(){
+  let name= document.querySelector('.name')
+    
+    
+    let res = localStorage.getItem("id")
+    name.innerHTML=`<h1>tufla${res}</h1>`;
+}
+async function itemicon(num){
+  
+  let item= document.querySelector(".item"+num)
+  let response = await fetch(`https://nevmerapp.herokuapp.com/users/${num}`)
+  let content = await response.json()
+  
+  
+  console.log("complite")
+  item.innerHTML=`<div ><img src="${content[0].img}"  alt="imgmenu"><p>${content[0].name}</p><a href="tabs/discr.html" ><button onclick="item(${num})">купить</button></a></div>`
+  
+}
+
+
+async function getResponse (){
+   
+  let num = localStorage.getItem("id")
+let response = await fetch(`https://nevmerapp.herokuapp.com/users/${num}`)
+let content = await response.json()
+console.log(content[0].name)
+
+
+let name= document.querySelector('.name')
+let size= document.querySelector('.size')
+let color= document.querySelector('.color')
+let disc= document.querySelector('.disc')
+let img= document.querySelector('.img')
+let cost= document.querySelector('.cost')
+
+
+
+name.innerHTML=`<h1>${content[0].name} </h1>`
+size.innerHTML=`<h1>Розмір: ${content[0].size} </h1>`
+color.innerHTML=`<h1>Колір: ${content[0].color} </h1>`
+disc.innerHTML=`<h1>Відгуки: ${content[0].disc} </h1>`
+img.innerHTML=`  <img src="${content[0].img}" alt="img">`
+cost.innerHTML=`<h1> Ціна: ${content[0].cost} грн</h1>`
+
+}
+
